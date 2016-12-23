2eab701bb052a2c262068b0dd9f5b25835625486
Ray <raimund.szabo@gmail.com>
2016-12-23 09:24:44 +0000

    - Better to do not use "Content-Lenght" at all as we cannot tell in advance what will be the size of the zip archive.

--- !download.php
+++ !download.php
@@ -1,18 +1,10 @@
 <?php 
 	$files = array("testFiles/1.jpg", "testFiles/2.jpg", "testFiles/3.jpg");
 
-	//Getting the total size of the files array
-	$totalSize = 0;
-	foreach ($files as $file) {
-		$totalSize += filesize($file);
-	}
-	$totalSize += 300; //I don't understand why, but the totalSize is always 300kb short of the correct size
-
 	header('Pragma: no-cache'); 
 	header('Content-Description: File Download'); 
 	header('Content-disposition: attachment; filename="myZip.zip"');
 	header('Content-Type: application/octet-stream');
-	header('Content-Length: ' . $totalSize);
 	header('Content-Transfer-Encoding: binary'); 
 
 	//Opening a zip stream
@@ -20,7 +12,7 @@
 	if ($files){
 		$fp = popen('zip -r -0 - ' . $files, 'r');
 	}
-	
+
 	flush(); //Flushing the butter, pre streaming
 	while(!feof($fp)) {
 	   	echo fread($fp, 8192);
